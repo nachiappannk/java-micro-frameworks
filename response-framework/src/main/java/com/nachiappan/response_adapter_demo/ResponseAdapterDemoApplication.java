6c8b161dd0930e526e78004d6e900df74a57fc26
Nachiappan NK <nachiapan@gmail.com>
2026-08-18 20:45:17 +0530

    Initial version of micro framework

---
+++ response-framework/src/main/java/com/nachiappan/response_adapter_demo/ResponseAdapterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nachiappan.response_adapter_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseAdapterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseAdapterDemoApplication.class, args);
+	}
+
+}
