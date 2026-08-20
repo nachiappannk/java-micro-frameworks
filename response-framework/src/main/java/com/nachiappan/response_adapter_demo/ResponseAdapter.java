c3eb1eeaf10a104d7f8f449aa15417c6eb01404a
Nachiappan NK <nachiapan@gmail.com>
2026-08-20 21:26:04 +0530

    Completed cleanup

--- response-framework/src/main/java/com/nachiappan/response_adapter_demo/ResponseAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.nachiappan.response_adapter_demo;
-
-public interface ResponseAdapter<T> {
-
-    T adapt(T body, CallerContext context);
-}
--- response-framework/src/main/java/com/nachiappan/response_adapter_demo/ResponseFrameworkConfig.java
+++
@@ -1,7 +0,0 @@
-package com.nachiappan.response_adapter_demo;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ResponseFrameworkConfig {
-}
